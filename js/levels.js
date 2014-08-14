3bf6efb3285cc6efbbc6c0ce719294851af7a7a5
peaceboy <peaceboy@users.noreply.github.com>
2014-08-14 23:54:19 +0530

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -1,17 +1,26 @@
 ﻿var levels = [];
 levels.push(new Level(0, 0, 0));
-levels.push(new Level(1, 1, 5));
-levels.push(new Level(2, 2, 10));
-levels.push(new Level(3, 4, 15));
-levels.push(new Level(4, 6, 20));
-levels.push(new Level(5, 10, 25));
-levels.push(new Level(6, 15, 30));
-levels.push(new Level(7, 18, 35));
-levels.push(new Level(8, 22, 40));
-levels.push(new Level(9, 30, 45));
-levels.push(new Level(10, 37, 50));
-levels.push(new Level(11, 48, 55));
-levels.push(new Level(12, 54, 60));
+levels.push(new Level(1, 1, 4));
+levels.push(new Level(2, 2, 6));
+levels.push(new Level(3, 4, 10));
+levels.push(new Level(4, 6, 15));
+levels.push(new Level(5, 8, 18));
+levels.push(new Level(6, 10, 20));
+levels.push(new Level(7, 14, 22));
+levels.push(new Level(8, 18, 28));
+levels.push(new Level(9, 20, 30));
+levels.push(new Level(10, 25, 35));
+levels.push(new Level(11, 28, 40));
+levels.push(new Level(12, 30, 45));
+levels.push(new Level(13, 35, 50));
+levels.push(new Level(14, 38, 53));
+levels.push(new Level(15, 42, 56));
+levels.push(new Level(16, 45, 60));
+levels.push(new Level(17, 48, 64));
+levels.push(new Level(18, 50, 68));
+levels.push(new Level(19, 55, 74));
+levels.push(new Level(20, 60, 80));
+
 
 function Level(number, requiredBalls, ballCount, settings) {
     this.number = number;
